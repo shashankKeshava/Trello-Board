8bdd902a5114c57e03c02d94e0a0f3340645b9a1
Shashank <keshavashashank@gmail.com>
2018-02-08 19:20:47 +0530

    Updates

--- src/utils/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const ADD_CARD = "ADD_CARD";
-export const REMOVE_CARD = "REMOVE_CARD";
-export const EDIT_CARD="EDIT_CARD";
-export const MOVE_VERTICAL ="MOVE_VERTICAL"
-export const MOVE_HORIZONTAL ="MOVE_HORIZONTAL"
