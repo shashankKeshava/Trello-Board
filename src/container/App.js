32a58f6b6a9b6ac57a2643aa94ae3b088ab8691f
Shashank <shashank.keshava@bookmyshow.com>
2018-02-08 17:42:39 +0530

    Minor Changes

--- src/container/App.js
+++ src/container/App.js
@@ -54,8 +54,9 @@ class App extends Component {
 }
 
 const mapToStateToProps = state => {
-    localStorage.setItem('trello', JSON.stringify(state));
-    let prevState = JSON.parse(localStorage.getItem('trello'));
+    sessionStorage.setItem('trello', JSON.stringify(state));
+    let prevState = JSON.parse(sessionStorage.getItem('trello'));
+    // console.log(prevState);
 
     return { data: state };
 };
